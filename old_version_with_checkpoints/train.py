af77032a52af3efaaecd4e5884a95f98580148b1
ilyak93 <ilyak93@gmail.com>
2021-03-10 00:27:57 +0200

    Update train.py

--- old_version_with_checkpoints/train.py
+++ old_version_with_checkpoints/train.py
@@ -3,10 +3,9 @@ import os
 import time
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from keras.metrics import Mean
-from keras.optimizers import Adam
-from keras.optimizers.schedules import ExponentialDecay
+from tensorflow.python.keras.metrics import Mean
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
 
 import functions
 from imresize import my_imresize
