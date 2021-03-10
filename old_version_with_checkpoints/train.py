b12eb199c9674b683aa14aeffe54a4542a041e81
ilyak93 <ilyak93@gmail.com>
2021-03-10 08:24:57 +0200

    Update train.py

--- old_version_with_checkpoints/train.py
+++ old_version_with_checkpoints/train.py
@@ -3,7 +3,7 @@ import os
 import time
 import numpy as np
 import tensorflow as tf
-from tensorflow.compat.v1.metrics import mean
+from tf.compat.v1.keras.metrics import Mean
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.optimizers.schedules import ExponentialDecay
 
